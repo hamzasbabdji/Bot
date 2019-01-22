06d4868811fed06bac9147fa79dcfab94947073f
hamzasbabdji <46747747+hamzasbabdji@users.noreply.github.com>
2019-01-22 13:16:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,27 +3,36 @@ const client = new Discord.Client();
 const client2 = new Discord.Client();
 const myid = [''];
 const prefix = ['-'];
-
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '.'
+ 
 client.on('ready', () => {
-   console.log(`----------------`);
-   console.log(`Credit Farmm - Script By : Kahrbaa `);
-   console.log(`----------------`);
-   console.log(`Loadinng`);
-   console.log(`Loadinng.`);
-   console.log(`Loadinng..`);
-   console.log(`Loadinng...`);
-   console.log(`Bot Is Online ' `);
-   console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
-
-client2.on('ready', () => {
-   console.log(`----------------`);
-   console.log(`Bot is Online ✩' `);
-   console.log(`----------------`);
-});
-
-
-
+ 
 client.on('message', message => {
     if(message.content === prefix+'راتب'){
         message.channel.send('#daily')
